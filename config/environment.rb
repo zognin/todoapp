58804faad50d1a683e33d3e3e8314013617e6397
Zoe <gengninghan1@gmail.com>
2021-01-11 10:44:07 +0800

    Worked on action mailer

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => 'apikey',
+  :password => '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
+',
+  :domain => 'yourdomain.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,6 +13,18 @@ Rails.application.configure do
   config.consider_all_requests_local = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  
+  # config.action_mailer.delivery_method = :smtp
+
+  # config.action_mailer.smtp_settings = {
+  #   address:'smtp.gmail.com',
+  #   port: 587,
+  #   domain: ENV['GMAIL_DOMAIN'],
+  #   authentication: 'plain',
+  #   enable_starttls_auto: true,
+  #   user_name: ENV['GMAIL_USERNAME'],
+  #   password: ENV['GMAIL_PASSWORD']
+  # }
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
