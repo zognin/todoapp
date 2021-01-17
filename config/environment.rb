0a487669608083947a661078be713a02e1241160
Zoe <gengninghan1@gmail.com>
2021-01-17 09:16:39 +0800

    changed mailer username and password

--- config/environment.rb
+++ config/environment.rb
@@ -5,9 +5,8 @@ require_relative 'application'
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  :user_name => 'apikey',
-  :password => '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
-',
+  :user_name => ENV['SENDGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
   :domain => 'https://zognin-todoapp-rails.herokuapp.com',
   :address => 'smtp.sendgrid.net',
   :port => 587,
